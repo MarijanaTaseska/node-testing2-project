0da2207c22339f08cbf983031338c88e5ca05326
Marijana Taseska <marijana.taseska@gmail.com>
2024-05-24 08:46:20 -0700

    error endpoint

--- api/data-Router.js
+++ api/data-Router.js
@@ -1,8 +1,16 @@
 const router = require('express').Router()
 
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
     res.json('data router is UPUPUP')
+})
+
+router.use((err, req, res, next) => {
+    res.status(500).json({
+        customMessage:'something went wrong inside data router',
+        stack:err.stack,
+        message:err.message,
+    })
 })
 
 module.exports= router
